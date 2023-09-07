c72a6e13774e7fd80dcb224a3fab60b57f643e40
msm <myatsumon@ucsy.edu.mm>
2023-09-07 11:00:19 +0630

    deleting  product

--- controllers/admin.js
+++ controllers/admin.js
@@ -84,8 +84,9 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  Product.findByIdAndRemove(prodId)
     .then(() => {
+      console.log("Destroyed Product");
       res.redirect("/admin/products");
     })
     .catch((err) => console.log(err));
