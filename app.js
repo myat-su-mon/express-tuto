f817dc472ce37b8fbb1e7c83bf977c4672790d0d
msm <myatsumon@ucsy.edu.mm>
2023-09-08 12:47:04 +0630

    adding user and user model

--- app.js
+++ app.js
@@ -14,14 +14,14 @@ app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-// app.use((req, res, next) => {
-//   User.findById("64f6c5ff6230729e3f9ab1d4")
-//     .then((user) => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("64faba69750a7e6d015499a6")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -31,16 +31,26 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-// mongoConnect(() => {
-//   app.listen(3000, () => console.log("server listening on port 3000"));
-// });
-
 mongoose
   .connect(
     "mongodb+srv://root:root@cluster0.j30ddxo.mongodb.net/shop?retryWrites=true&w=majority",
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => {
-    app.listen(3000);
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Aung",
+          email: "aung@gmail.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+
+    console.log("MongoDB is connected");
+    app.listen(3000, () => console.log("server listening on port 3000"));
   })
   .catch((err) => console.log(err));
