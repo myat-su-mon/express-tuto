e22cdd403535497b35eed59d5826e7e52e874363
msm <myatsumon@ucsy.edu.mm>
2023-09-11 09:20:03 +0630

    route protection with middleware

--- routes/shop.js
+++ routes/shop.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const shopController = require("../controllers/shop");
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
@@ -10,17 +11,21 @@ router.get("/", shopController.getIndex);
 router.get("/products", shopController.getProducts);
 
 // // dynamic segment
-router.get("/products/:productId", shopController.getProduct);
+router.get("/products/:productId", isAuth, shopController.getProduct);
 
-router.get("/cart", shopController.getCart);
+router.get("/cart", isAuth, shopController.getCart);
 
-router.post("/cart", shopController.postCart);
+router.post("/cart", isAuth, shopController.postCart);
 
-router.post("/cart-delete-product", shopController.postCartDeleteProduct);
+router.post(
+  "/cart-delete-product",
+  isAuth,
+  shopController.postCartDeleteProduct
+);
 
-router.get("/orders", shopController.getOrders);
+router.get("/orders", isAuth, shopController.getOrders);
 
-router.post("/create-order", shopController.postOrder);
+router.post("/create-order", isAuth, shopController.postOrder);
 
 // router.get("/checkout", shopController.getCheckout);
 
