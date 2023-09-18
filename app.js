7f76cd9484c725f7669df98505040bdcf8608dd2
msm <myatsumon@ucsy.edu.mm>
2023-09-18 14:49:49 +0630

    returning error pages

--- app.js
+++ app.js
@@ -50,6 +50,9 @@ app.use((req, res, next) => {
   }
   User.findById(req.session.user._id)
     .then((user) => {
+      if (!user) {
+        return next();
+      }
       req.user = user;
       next();
     })
@@ -66,6 +69,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+app.get("/500", errorController.get500);
 app.use(errorController.get404);
 
 mongoose
--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,6 @@
 const Product = require("../models/product");
 const { validationResult } = require("express-validator");
+const mongoose = require("mongoose");
 
 exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
@@ -38,6 +39,7 @@ exports.postAddProduct = (req, res, next) => {
   }
 
   const product = new Product({
+    _id: new mongoose.Types.ObjectId("650801c3f29cbe08652f88a8"),
     title: title,
     price: price,
     description: description,
@@ -51,7 +53,8 @@ exports.postAddProduct = (req, res, next) => {
       res.redirect("/admin/products");
     })
     .catch((err) => {
-      console.log(err);
+      console.log("An error occurred.");
+      res.redirect("/500");
     });
 };
 
