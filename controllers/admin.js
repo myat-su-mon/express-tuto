c2c7c3647c1c788a34936cee199b753470a1642b
msm <myatsumon@ucsy.edu.mm>
2023-09-19 10:22:38 +0630

    storing file data in db 1

--- controllers/admin.js
+++ controllers/admin.js
@@ -18,9 +18,24 @@ exports.postAddProduct = (req, res, next) => {
   const image = req.file;
   const price = req.body.price;
   const description = req.body.description;
-  const errors = validationResult(req);
-  console.log(image);
 
+  if (!image) {
+    return res.status(422).render("admin/edit-product", {
+      pageTitle: "Add Product",
+      path: "/admin/edit-product",
+      editing: false,
+      hasError: true,
+      product: {
+        title: title,
+        price: price,
+        description: description,
+      },
+      errorMessage: "Attached file is not an image.",
+      validationErrors: [],
+    });
+  }
+
+  const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors.array());
     return res.status(422).render("admin/edit-product", {
@@ -39,6 +54,8 @@ exports.postAddProduct = (req, res, next) => {
     });
   }
 
+  const imageUrl = image.path;
+  console.log(imageUrl, "imageurl");
   const product = new Product({
     title: title,
     price: price,
