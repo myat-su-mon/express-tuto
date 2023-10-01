10d3020552f93d2f23e68caf591bb0612916dde4
msm <myatsumon@ucsy.edu.mm>
2023-10-01 10:58:21 +0630

    generating pdf files with order data

--- controllers/shop.js
+++ controllers/shop.js
@@ -143,7 +143,27 @@ exports.getInvoice = (req, res, next) => {
       res.setHeader("Content-Disposition", `inline; filename=${invoiceName}`);
       pdfDoc.pipe(fs.createWriteStream(invoicePath));
       pdfDoc.pipe(res);
-      pdfDoc.text("Hello World");
+      // pdfDoc.text("Hello World");
+      pdfDoc.fontSize(26).text("Invoice", {
+        underline: true,
+      });
+      pdfDoc.text("-----------------------");
+      let totalPrice = 0;
+      order.products.forEach((prod) => {
+        totalPrice += prod.quantity * prod.product.price;
+        pdfDoc
+          .fontSize(14)
+          .text(
+            prod.product.title +
+              " - " +
+              prod.quantity +
+              " x " +
+              "$" +
+              prod.product.price
+          );
+      });
+      pdfDoc.text("-----------------------");
+      pdfDoc.fontSize(20).text("Total Price: $" + totalPrice);
       pdfDoc.end();
     })
     .catch((err) => next(err));
